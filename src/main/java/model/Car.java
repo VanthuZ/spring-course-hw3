81c70640684f96abde9dc2209120b39252b394bb
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-09-30 22:01:52 +0200

    Changed color type from string to enum

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import model.enums.Color;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Car {
 
     private long id;
     private String mark;
     private String model;
-    private String color;
+    private Color color;
 }
---
+++ src/main/java/model/enums/Color.java
@@ -0,0 +1,8 @@
+package model.enums;
+
+public enum Color {
+    red,
+    white,
+    black,
+    blue
+}
